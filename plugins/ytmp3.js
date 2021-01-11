b79dc9e6d1a9c929e54342bb8606159cf0c18826
ubednihboss <77176442+ubednihboss@users.noreply.github.com>
2021-01-11 11:45:44 +0700

    update

---
+++ plugins/ytmp3.js
@@ -0,0 +1,15 @@
+const axios = require('axios')
+const link = 'https://arugaz.herokuapp.com'
+const fileyt = 'https://raw.githubusercontent.com/ArugaZ/scraper-results/main/20201111_230923.jpg'
+const eroryt = 'https://raw.githubusercontent.com/ArugaZ/scraper-results/main/20201111_234624.jpg'
+
+
+const ytmp3 = async (url) => new Promise((resolve, reject) => {
+    axios.get(`${link}/api/yta?url=${url}`)
+    .then((res) => {
+		resolve(res.data)
+    })
+    .catch((err) =>{
+        reject(err)
+    })
+
