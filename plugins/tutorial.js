dd21728d0087483f583f874c5d9e81178bd71f32
ubednihboss <77176442+ubednihboss@users.noreply.github.com>
2021-01-11 10:38:35 +0700

    Tutor

--- plugins/tutorial.js
+++ plugins/tutorial.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
 
 
-KEPO LOH 
+KEPO LOH ANJINK
 
 
 WOWKWOWKWOK
@@ -12,3 +12,22 @@ WOWKWOWKWOK
 
 By: ubed
 wa.me/6281358679254
+
+
+
+
+`.trim(), m)
+}
+handler.command = /^(tutorial)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
